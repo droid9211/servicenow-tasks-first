c4effac032fab3dc9ef02b38b61ec3ddb7ec217e
Lilly A. Nikolova <96025384+droid9211@users.noreply.github.com>
2022-07-09 01:52:13 +0300

    Add files via upload
    made javascript changes

--- main.js
+++ main.js
@@ -1,54 +1,65 @@
-////////////////  1 /////////////
-var up = document.getElementById('GFG_UP');
-var down = document.getElementById('GFG_DOWN');
-var n = 5;
-         
-up.innerHTML = "Click on the button to calculate" + " the factorial of n.<br>n = " + n;
-     
-function Factorial(n) {
-    var ans=1;       
-    for (var i = 2; i <= n; i++)
-        ans = ans * i;
-        return ans;
-}
-         
-function GFG_Fun() {
-        down.innerHTML = Factorial(n);
-}
-
-/////////////////// 2 ////////////////
-
-function palindrome() {
-	var a= document.getElementById("pa").value;
-	var b = "";
-	for (i = a.length-1; i >= 0; i--) {
-		b = b + a[i]
-	}
-	if (a == b)
-		document.getElementById("pa2"). value = b + " is a Palindrome String ";
-	else
-		document.getElementById("pa2"). value = b + " is not a Palindrome String";
+function factorial(){
+    var output = document.getElementById("output-el")
+var inputEl = document.getElementById("input-el")
+    var input = document.getElementById("input-el").value.trim()
+    let answer = 1;
+    if (input == 0 || input == 1){
+      return ;
+    }else{
+      for(var i = input; i >= 1; i--){
+        answer = answer * i;
+      }
+     output.innerHTML = '<p>'+answer+'</p>';
+     inputEl.value = ""
+      
+    }  
 }
 
-/////////////////// 3 ////////////////
-
-function uniqueCharacters(str)
-{
-     
-    for(let i = 0; i < str.length; i++)
-        for(let j = i + 1; j < str.length; j++)
-            if (str[i] == str[j])
-                return false;
-    return true;
+function evenOccurences(){
+    var inputEvenEl = document.getElementById("input-el-even").value
+    var outputEvenEl = document.getElementById("output-el-even")
+    var final = {}
+    if(inputEvenEl.length <= 0){
+        
+        outputEvenEl.innerHTML = 'Invalid input'
+        return
+    }
+    for(var i=0; i<inputEvenEl.length;i++){
+        if(!final[inputEvenEl[i]]){
+            final[inputEvenEl[i]] = 1
+        }else {
+            final[inputEvenEl[i]]++
+        }
+    }
+    for(var key in final){
+        var counter = final[key]
+        if(counter % 2 == 1){
+            outputEvenEl.innerHTML = 'false'
+            return
+        }
+    }
+    outputEvenEl.innerHTML = 'true'
 }
 
-function unique1() {
-var str = document.getElementById("is-unique").value;
+function palindrome(){
+    var inputElement = document.getElementById("input-el-palindrome").value.trim()
+    var outputEl = document.getElementById("output-el-palindrome")
+    var reversedInput = inputElement.split("").reverse().join("")
+    if(inputElement === reversedInput){
+        outputEl.innerHTML = '<p>'+inputElement+' is a plaindrome</p>'
+    }else {
+        outputEl.innerHTML = '<p>'+inputElement+' is NOT a plaindrome</p>'
 
-if (uniqueCharacters(str) == true)
-    document.getElementById("result").innerHTML= str +
-                   " has all unique characters" ;
-else
-    document.getElementById("result").innerHTML= str +
-                   " has duplicate characters";
+    }
 }
+
+var closeBtn = document.querySelector(".close-add")
+var adBanner = document.querySelector(".top-banner")
+closeBtn.addEventListener("click", function() {
+    adBanner.classList.toggle("hide")
+    var closedAd = document.querySelector(".closed-add")
+    if(adBanner.style.display = "none"){
+        closedAd.classList.toggle("hide-closed-add")
+    }
+
+})
